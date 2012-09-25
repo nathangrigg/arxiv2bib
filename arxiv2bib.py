f58e6ccb912ee671b6cd51ed20ee6e8475c32525
Nathan Grigg <nathan@nathanamy.org>
2012-09-25 12:32:13 -0700

    Hard code old style regex
    This isn't going to change, and this is less confusing.

--- arxiv2bib.py
+++ arxiv2bib.py
@@ -50,41 +50,38 @@ ATOM ='{http://www.w3.org/2005/Atom}'
 ARXIV = '{http://arxiv.org/schemas/atom}'
 
 # regular expressions to check if arxiv id is valid
-def sub_cat_re(cat, subcats):
-    """Create a regular expression for all subcategories of a category"""
-    if subcats:
-        return r'{0}(\.({1}))?'.format(cat, '|'.join(subcats.split()))
-    else:
-        return cat
-
-
-SUBCATEGORIES = {
-    'stat': 'AP CO ML ME TH',
-    'q-bio': 'BM CB GN MN NC OT PE QM SC TO',
-    'cs': 'AR AI CL CC CE CG GT CV CY CR DS DB DL DM DC GL GR HC IR IT ' +
-      'LG LO MS MA MM NI NE NA OS OH PF PL RO SE SD SC',
-    'nlin': 'AO CG CD SI PS',
-    'math': 'AG AT AP CT CA CO AC CV DG DS FA GM GN GT GR HO IT KT LO MP ' +
-      'MG NT NA OA OC PR QA RT RA SP ST SG',
-    'astro-ph': '',
-    'cond-mat': 'dis-nn mes-hall mtrl-sci other soft stat-mech str-el supr-con',
-    'gr-qc': '',
-    'hep-ex': '',
-    'hep-lat': '',
-    'hep-ph': '',
-    'hep-th': '',
-    'math-ph': '',
-    'nucl-ex': '',
-    'nucl-th': '',
-    'quant-ph': '',
-    'physics': 'acc-ph ao-ph atom-ph atm-clus bio-ph chem-ph class-ph comp-ph ' +
-        'data-an flu-dyn gen-ph geo-ph hist-ph ins-det med-ph optics ed-ph ' +
-        'soc-ph plasm-ph pop-ph space-ph',
-}
-OLD_STYLE = re.compile('^(' +
-  '|'.join(sub_cat_re(cat, subcats) for cat, subcats in SUBCATEGORIES.items())
-   + ')/\d{7}(v\d+)?$')
 NEW_STYLE = re.compile(r'^\d{4}\.\d{4}(v\d+)?$')
+OLD_STYLE = re.compile(r"""(?x)
+^(
+   math-ph
+  |hep-ph
+  |nucl-ex
+  |nucl-th
+  |gr-qc
+  |astro-ph
+  |hep-lat
+  |quant-ph
+  |hep-ex
+  |hep-th
+  |stat
+    (\.(AP|CO|ML|ME|TH))?
+  |q-bio
+    (\.(BM|CB|GN|MN|NC|OT|PE|QM|SC|TO))?
+  |cond-mat
+    (\.(dis-nn|mes-hall|mtrl-sci|other|soft|stat-mech|str-el|supr-con))?
+  |cs
+    (\.(AR|AI|CL|CC|CE|CG|GT|CV|CY|CR|DS|DB|DL|DM|DC|GL|GR|HC|IR|IT|LG|LO|
+      MS|MA|MM|NI|NE|NA|OS|OH|PF|PL|RO|SE|SD|SC))?
+  |nlin
+    (\.(AO|CG|CD|SI|PS))?
+  |physics
+    (\.(acc-ph|ao-ph|atom-ph|atm-clus|bio-ph|chem-ph|class-ph|comp-ph|
+      data-an|flu-dyn|gen-ph|geo-ph|hist-ph|ins-det|med-ph|optics|ed-ph|
+      soc-ph|plasm-ph|pop-ph|space-ph))?
+  |math
+      (\.(AG|AT|AP|CT|CA|CO|AC|CV|DG|DS|FA|GM|GN|GT|GR|HO|IT|KT|LO|MP|MG
+      |NT|NA|OA|OC|PR|QA|RT|RA|SP|ST|SG))?
+)/\d{7}(v\d+)?$""")
 
 def is_valid(arxiv_id):
     """Checks if id resembles a valid arxiv identifier."""
